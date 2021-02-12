bef9fe4919e6a6a845e0b57aaae46897b563c3a5
fentyhall <fentymhall@gmail.com>
2021-02-12 15:21:34 -0800

    commit tree 6fb90f229b74f965db94e8195a58ca55bb132f03

--- main.js
+++ main.js
@@ -2,10 +2,36 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let glyphStates = {
+  "♡": "♥",
+  "♥": "♡"
+};
 
+let colorStates = {
+  "red": "",
+  "": "red"
+};
 
+let articleHearts = document.getElementsByClassName("like")
 
+for (let eachHeart of articleHearts) {
+  eachHeart.addEventListener("click", likeHeart);
+}
+
+function likeHeart(event) {
+  let heart = event.target;
+  mimicServerCall()
+    .then( () => {
+      heart.innerText = glyphStates[heart.innerText];
+      heart.style.color = colorStates[heart.style.color];
+    })
+    .catch( () => {
+      document.getElementById("modal").className = "";
+      setTimeout(function() {
+        document.getElementById("modal").className = "hidden";
+      }, 5000);
+    });
+}
 
 //------------------------------------------------------------------------------
 // Ignore after this point. Used only for demo purposes
